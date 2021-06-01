d6ebbec0d7e6b4dce9b6f82ebd1518d9b3c3a637
CKyang <pithend58639@gmail.com>
2021-06-02 01:43:46 +0800

    create wip

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,3 +1,30 @@
 class WordsController < ApplicationController
-  def index; end
+  def index
+    @word = Word.all
+    @chinese = @word.pluck(:chinese)
+    @korean = @word.pluck(:korean)
+  end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(clean_params)
+    @word.save!
+    redirect_to '/words'
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def clean_params
+    params.require(:word).permit(:chinese, :korean)
+  end
 end
