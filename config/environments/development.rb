c8b31261dcc566c2c7716d018cd00af3365e30fd
CKyang <pithend58639@gmail.com>
2021-06-03 00:25:07 +0800

    add chinese validate just can be chinese & model connection

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # rubocop:disable Lint/Debugger
+  console do
+    ActiveRecord::Base.connection
+  end
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
